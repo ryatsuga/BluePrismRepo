7b4dd400447170a315d0aaeb3369301b71b2121c
Leandro Passos <leandro.passos@elogroup.com.br>
2023-01-23 14:00:48 -0300

    Import and export bat files created

---
+++ releasedeploy.bat
@@ -0,0 +1,2 @@
+"C:\Program Files\Blue Prism Limited\Blue Prism Automate\AutomateC.exe" /importrelease
+TestGit.bprelease /overwrite /user %1 %2 /dbconname %3
---
+++ teste.txt
@@ -0,0 +1,2 @@
+"C:\Program Files\Blue Prism Limited\Blue Prism Automate\AutomateC.exe" /import "BPA Object -
+Test.xml" /overwrite /user %1 %2 /dbconname %3
